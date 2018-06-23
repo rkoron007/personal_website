8e01e839338cf0ea9204151b29ca08a26dca5fd6
Rose <rkoron007@gmail.com>
2018-06-22 23:01:11 -0700

    Started Webpack

---
+++ components/app.jsx
@@ -0,0 +1,9 @@
+import React from "React";
+
+class PersonalBox extends React.Component {
+    render(){
+        return (
+            <div>Hello</div>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, './lib');
+var APP_DIR = path.resolve(__dirname, './');
+
+var config = {
+    entry: "./index.js",
+    output: {
+        path: BUILD_DIR,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: [/\.jsx?$/],
+                include: APP_DIR,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['env', 'react', 'es2015']
+                }
+            }
+            // {
+            //     test: /\.css$/,
+            //     loader: "style-loader!css-loader"
+            // }
+        ],
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+};
+
+module.exports = config;
